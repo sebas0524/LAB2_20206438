2a085e400b64982586c1a3af02f80a9af491c1b3
Sebastian <a20206438@pucp.edu.pe>
2025-04-06 23:25:22 -0500

    pagina de incio

--- LAB2_20206438/app/src/main/java/com/example/lab2_20206438/MainActivity.java
+++ LAB2_20206438/app/src/main/java/com/example/lab2_20206438/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.lab2_20206438;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSoftware, btnCiberseguridad, btnOpticas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.pagina_inicio);
+
+        // Referenciar botones
+        btnSoftware = findViewById(R.id.btnSoftware);
+        btnCiberseguridad = findViewById(R.id.btnCiberseguridad);
+        btnOpticas = findViewById(R.id.btnOpticas);
+
+        // Luego puedes agregar acciones aquí si quieres
+        // Por ejemplo:
+        /*
+        btnSoftware.setOnClickListener(v -> {
+            // Acción para Software
         });
+        */
     }
 }
